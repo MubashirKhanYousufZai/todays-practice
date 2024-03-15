2f2c2a934776e62072d657bee37d7bcbc1cb561d
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-03-15 06:52:14 +0500

    my todays practice

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstName = "Mubashir";
+var age = 15;
+console.log("my name if ".concat(firstName, " and my age is: ").concat(age));
+var f = 8;
+var result = ++f + f++ + 17 + --f - 2;
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let firstName :string = "Mubashir";
+let age : number = 15;
+console.log(`my name if ${firstName} and my age is: ${age}`)
+let f =8;
+let result = ++f + f++ + 17 + --f -2; // 9+10+17-8-2
+console.log(result);
